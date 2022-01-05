5e8fa178b72d58338df3c5f5471a420763c8bd99
okok0415 <dlawjdals11@naver.com>
2022-01-05 15:37:09 +0900

    #4.0 Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 
-import React, { useState } from "react";
-import styled, { ThemeProvider } from "styled-components";
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-
-`
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`
+import Router from "./Router";
 
 
 function App() {
   return (
-    <Container>
-      <H1>protected</H1>
-    </Container>
-  );
-}
-
+    <Router/>
+  )
+  }
 export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,19 @@
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
+import Coin from "./routes/Coin";
+import Coins from "./routes/Coins";
+
+
+function Router() {
+    return (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/:coinId"><Coin/></Route>
+            <Route path="/"><Coins /></Route>
+        </Switch>
+    </BrowserRouter>
+    )
+}
+
+export default Router;
